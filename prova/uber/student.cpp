5c471284b8145f09b8142370db8908781b6a8622
Paulo-Sergio-Oliveira <paulosergiooliveira113@gmail.com>
2024-12-05 11:34:48 -0300

    Resolução Prova

--- prova/uber/student.cpp
+++ prova/uber/student.cpp
@@ -1,23 +1,116 @@
 #include <iostream>
 #include "fn.hpp"
+using namespace std;
 
+class Pessoa {
+    string nome;
+    int dinheiro;
+
+    public:
+
+        Pessoa (string nome, int dinheiro): nome(nome), dinheiro(dinheiro) {}
+
+        string getNome() {
+            return this->nome;
+        }
+
+        void setNome(string nome) {
+            this->nome = nome;
+        }
+
+        int getDinheiro() {
+            return this->dinheiro;
+        }
+
+        void setDinheiro(int dinheiro) {
+            this->dinheiro = dinheiro;
+        }
+
+        string str() {
+            return fn::format("{}:{}", this->nome, this->dinheiro);
+        }
+};
+
+class Moto {
+    int custo;
+    shared_ptr<Pessoa> motorista;
+    shared_ptr<Pessoa> passageiro;
+
+    public:
+
+        Moto(int custo, shared_ptr<Pessoa> motorista, shared_ptr<Pessoa> passageiro): 
+        custo(custo), motorista(motorista), passageiro(passageiro) {}
+
+        string str() {
+            return fn::format("Cost: {}, Driver: {}, Passenger: {}\n",
+            this->custo, (this->motorista != nullptr ? this->motorista->str() : "None"),
+            (this->passageiro != nullptr ? this->passageiro->str() : "None"));
+        }
+
+        void setDriver(string nome, int dinheiro) {
+            if(this->motorista == nullptr) {
+                this->motorista = make_shared<Pessoa>(nome, dinheiro);
+                return;
+            }
+
+            cout << "fail: There is already a driver" << endl;
+        }
+
+        void setPass(string nome, int dinheiro) { 
+            if(this->passageiro == nullptr) {
+                this->passageiro = make_shared<Pessoa>(nome, dinheiro);
+                return;
+            }
+
+            cout << "fail: There is already a passenger" << endl;
+        }
+
+        void drive(int distancia) {
+            if(this->motorista != nullptr) {
+                this->custo += distancia;
+            }
+            
+            cout << "fail: There is no driver to drive" << endl;
+        }
+
+        void leavePass() {
+            if(this->passageiro != nullptr) {
+                this->motorista->setDinheiro(this->motorista->getDinheiro() + this->custo);
+                this->passageiro->setDinheiro(this->passageiro->getDinheiro() - this->custo);
+                this->custo = 0;
+                
+                if (this->passageiro->getDinheiro() < 0) {
+                    cout << "fail: Passenger does not have enough money" << endl;
+                    this->passageiro->setDinheiro(0);
+                }
+                
+                cout << this->passageiro->getNome() << ":" << this->passageiro->getDinheiro() << " leave" << endl;
+                this->passageiro = nullptr;
+                return;
+            }
+
+            cout << "fail: There is no passenger to leave" << endl;
+        }
+};
 
 class Adapter {
 public:
-    void setDriver(std::string name, int money) {
-        (void) name;
-        (void) money;
+    Moto moto = Moto(0, nullptr, nullptr);
+
+    void setDriver(string name, int money) {
+        moto.setDriver(name, money);
     }
-    void setPass(std::string name, int money) {
-        (void) name;
-        (void) money;
+    void setPass(string name, int money) {
+        moto.setPass(name, money);
     }
     void drive(int distance) {
-        (void) distance;
+        moto.drive(distance);
     }
     void leavePass() {
+        moto.leavePass();
     }
     void show() {
+        cout << moto.str();
     }
 };
 
